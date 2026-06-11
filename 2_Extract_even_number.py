6018fa74bd4cfd97a6182a8c7545e784d63a88b8
kishormechatronic <kishormechatronic@gmail.com>
2026-06-11 08:27:53 +0545

    first commit

---
+++ 2_Extract_even_number.py
@@ -0,0 +1,3 @@
+lst=[10,15,20,25,30,35,40]
+ans=[val for val in lst if val%2==0]
+print(ans)
---
+++ 3_upper.py
@@ -0,0 +1,3 @@
+lst=["alice","bob","charlie","david"]
+cc=[i.upper() for i in lst ]
+print("uppercase: ",cc)
