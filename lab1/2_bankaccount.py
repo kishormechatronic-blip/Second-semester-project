b386494d2033a46b95b988d9295f6a0aa841a128
kishormechatronic <kishormechatronic@gmail.com>
2026-09-05 10:58:23 +0545

    Add lab 1

---
+++ lab1/2_bankaccount.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+
+
+account1 = BankAccount("Alice", 500)
+account2 = BankAccount("Bob")  # default balance = 0
+
+print("Account 1 owner:", account1.owner, "Balance:", account1.balance)
+print("Account 2 owner:", account2.owner, "Balance:", account2.balance)
